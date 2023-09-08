a096d3cca3a8e4188c5552a47a84f4952de51156
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-09-08 15:14:16 +0300

    Added handler for EntityNotFoundException

--- src/main/java/com/example/onlinebookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/onlinebookstore/exception/CustomGlobalExceptionHandler.java
@@ -10,11 +10,14 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final int NOT_FOUND_STATUS = 404;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
@@ -31,6 +34,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         notValidResponse.setStatus(HttpStatus.BAD_REQUEST);
         notValidResponse.setErrors(errors);
         return new ResponseEntity<>(notValidResponse, headers, status);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFoundException(
+            EntityNotFoundException e
+    ) {
+        ArgumentNotValidResponse notValidResponse = new ArgumentNotValidResponse();
+        notValidResponse.setTimestamp(LocalDateTime.now());
+        notValidResponse.setStatus(HttpStatus.NOT_FOUND);
+        notValidResponse.setErrors(new String[]{e.getMessage()});
+        return new ResponseEntity<>(notValidResponse, HttpStatusCode.valueOf(NOT_FOUND_STATUS));
     }
 
     private String getErrorMessage(ObjectError e) {

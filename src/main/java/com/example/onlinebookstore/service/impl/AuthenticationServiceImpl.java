691ac211f31ab6b4525f24bfec76c784f6232bb3
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-09-12 23:41:01 +0300

    Fixed passwords

--- src/main/java/com/example/onlinebookstore/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/AuthenticationServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.onlinebookstore.service.impl;
 
 import com.example.onlinebookstore.dto.user.UserLoginRequestDto;
 import com.example.onlinebookstore.dto.user.UserLoginResponseDto;
+import com.example.onlinebookstore.model.User;
+import com.example.onlinebookstore.repository.user.UserRepository;
 import com.example.onlinebookstore.security.JwtUtil;
 import com.example.onlinebookstore.service.AuthenticationService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,10 +18,12 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     @Override
     public UserLoginResponseDto authenticate(UserLoginRequestDto request) {
-        Authentication authentication = authenticationManager.authenticate(
+        Optional<User> byEmail = userRepository.findByEmail(request.getEmail());
+        final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         String token = jwtUtil.generateToken(authentication.getName());
         return new UserLoginResponseDto(token);

dbf87890a7959056fc3fca639e8354762b2cc41d
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-09-14 16:55:33 +0300

    PreMerge

--- src/main/java/com/example/onlinebookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/book/CreateBookRequestDto.java
@@ -1,24 +1,24 @@
 package com.example.onlinebookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
+import java.util.Set;
+
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
     @NotNull
     @NotBlank
-    @Length(min = 1, max = 255)
     @Size(min = 1, max = 255)
     private String title;
     @NotNull
     @NotBlank
-    @Length(min = 1, max = 255)
     @Size(min = 1, max = 255)
     private String author;
     @NotNull

2bc9b998adf7bc297245e81fef72e2b645a82885
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-09-14 16:41:26 +0300

    Deleted redundant method

--- src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
@@ -51,7 +51,9 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto update(Long id, CreateBookRequestDto bookRequestDto) {
-        orElseThrowIfPresent(id);
+        if (!bookRepository.existsById(id)) {
+            throw new EntityNotFoundException("Can't find book with id: " + id);
+        }
         Book bookForUpdate = bookMapper.toModel(bookRequestDto);
         bookForUpdate.setId(id);
         return bookMapper.toDto(bookRepository.save(bookForUpdate));
@@ -63,11 +65,5 @@ public class BookServiceImpl implements BookService {
         return bookRepository.findAll(bookSpecification, pageable).stream()
                 .map(bookMapper::toDto)
                 .collect(Collectors.toList());
-    }
-
-    private void orElseThrowIfPresent(Long id) {
-        if (!bookRepository.existsById(id)) {
-            throw new EntityNotFoundException("Can't find book with id: " + id);
-        }
     }
 }

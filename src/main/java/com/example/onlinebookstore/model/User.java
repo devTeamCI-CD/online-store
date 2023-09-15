1664b330fb069a4d521dc7c609dfa5e0392dd075
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-09-15 16:57:34 +0300

    Added imports ToString and Equals

--- src/main/java/com/example/onlinebookstore/model/User.java
+++ src/main/java/com/example/onlinebookstore/model/User.java
@@ -14,6 +14,8 @@ import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Set;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
